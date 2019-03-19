6a01e8e48cfe19f62d3d84c4d9e6933bfad81b6b
bigman <daniel.raban@gmail.com>
2019-03-19 22:09:04 +0200

    * removed local project files for SysHooks and SysInvoke * added nuget project reference for SysHooks and SysInvoke

--- SysHooks/Interfaces/IHookCallback.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace RabanSoft.SysHooks.Interfaces {
-    public interface IHookCallback {
-        void OnError(Exception ex);
-        void OnHookProc(int nCode, IntPtr wParam, IntPtr lParam);
-    }
-}
