536f1b151f97b60d0a651e5f5598796c8ffcb15e
bigman <daniel.raban@gmail.com>
2019-03-19 19:26:49 +0200

    updated project information

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("ClipboardExtension.Core")]
+[assembly: AssemblyDescription("Core modules for Clipboard Extension application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Core")]
+[assembly: AssemblyCompany("RabanSoft.")]
+[assembly: AssemblyProduct("Windows Clipboard Manager")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("RabanSoft.")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
--- SysHooks/Properties/AssemblyInfo.cs
+++ SysHooks/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SysHooks")]
+[assembly: AssemblyTitle("RabanSoft.SysHooks")]
 [assembly: AssemblyDescription("Basic windows hook implementation library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RabanSoft.")]
